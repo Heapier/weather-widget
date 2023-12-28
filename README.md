2961a442934f114c3baf32e6253437bbaa06fc78
Alif Fatul <alif@aliffatulmf.my.id>
2023-12-28 11:50:12 +0700

    'init'

---
+++ README.md
@@ -0,0 +1,19 @@
+# Weather Application
+
+This is a simple weather application built with Flask and the WeatherAPI.
+
+## Features
+
+- Get current weather and forecast for the next 5 days.
+- User can input their location to get the weather data.
+
+## Installation
+
+1. Clone this repository.
+2. Install the dependencies with `pip install -r requirements.txt`.
+3. Set your WeatherAPI key as an environment variable with `export WEATHER_API_KEY=your_api_key` (Unix/Linux) or `set WEATHER_API_KEY=your_api_key` (Windows).
+4. Run the application with `python app.py`.
+
+## Usage
+
+Open your web browser and navigate to `http://localhost:5000`. Enter your location in the form and submit to get the weather data.
---
+++ flask_app.py
@@ -0,0 +1,123 @@
+import os
+from datetime import datetime
+
+import requests
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+
+load_dotenv()  # Memuat variabel lingkungan dari file .env
+
+app = Flask(__name__, template_folder="templates")
+
+def get_weather_data(query="Semarang"):
+    # URL API untuk mendapatkan data cuaca
+    url = "https://api.weatherapi.com/v1/forecast.json"
+    # Parameter yang dibutuhkan oleh API
+    params = {
+        # Mengambil kunci API dari environment variable
+        "key": os.getenv("WEATHER_API_KEY"),
+        "q": query,
+        "days": 5,
+        "aqi": "no",
+        "alerts": "no",
+    }
+    try:
+        # Melakukan request ke API
+        r = requests.get(url, params=params)
+        r.raise_for_status()  # Jika respons tidak berhasil, akan menimbulkan HTTPError
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+        return None
+    except requests.exceptions.RequestException as err:
+        print(f"Error occurred: {err}")
+        return None
+
+    try:
+        # Mengembalikan data dalam bentuk JSON
+        return r.json()
+    except ValueError:
+        print("Error: Unable to parse JSON response")
+        return None
+
+
+def process_data(location):
+    # Mendapatkan data cuaca dari lokasi yang diberikan
+    weather = get_weather_data(location)
+    data = {}
+
+    # Jika data cuaca tidak valid, keluar dan kembalikan None
+    if weather is None or not isinstance(weather, dict):
+        print("Error: Unable to process weather data")
+        return None
+
+    try:
+        # Mengubah format waktu dari data cuaca
+        d = datetime.strptime(
+            weather["current"]["last_updated"], "%Y-%m-%d %H:%M")
+        # Memperbarui data dengan informasi lokasi dan cuaca saat ini
+        data.update({
+            "location": {
+                "name": weather["location"]["name"],
+                "country": weather["location"]["country"],
+            },
+            "current": {
+                "temp": weather["current"]["temp_c"],
+                "condition": {
+                    "text": weather["current"]["condition"]["text"],
+                    "icon": weather["current"]["condition"]["icon"],
+                },
+                "datetime": {
+                    "day": d.strftime("%A"),
+                    "date": d.strftime("%d %B %Y"),
+                },
+            }
+        })
+
+        # Mempersiapkan daftar untuk ramalan cuaca
+        forecast = []
+
+        # Mengubah format waktu dan memperbarui ramalan cuaca untuk setiap hari
+        for i in weather["forecast"]["forecastday"]:
+            d = datetime.strptime(i["date"], "%Y-%m-%d")
+
+            forecast.append({
+                "datetime": {
+                    "day": d.strftime("%A"),
+                    "date": d.strftime("%d %B %Y"),
+                },
+                "condition": {
+                    "text": i["day"]["condition"]["text"],
+                    "icon": i["day"]["condition"]["icon"],
+                },
+                "temp": {
+                    "avgtemp": i["day"]["avgtemp_c"],
+                    "mintemp": i["day"]["mintemp_c"],
+                    "maxtemp": i["day"]["maxtemp_c"],
+                }
+            })
+
+        # Menambahkan forecast ke data
+        data.update({"forecast": forecast})
+
+    except KeyError as e:
+        # Menangani kesalahan jika kunci tidak ditemukan dalam data cuaca
+        print(f"Error: Key {e} not found in weather data")
+        return None
+
+    return data
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # Mendapatkan lokasi dari form, atau menggunakan 'Semarang' sebagai default
+    location = request.form.get("location", "Semarang").strip()
+
+    # Jika lokasi kosong, gunakan 'Semarang' sebagai lokasi
+    if location == "":
+        location = "Semarang"
+
+    # Mendapatkan data cuaca untuk lokasi yang diberikan
+    weather_data = process_data(location)
+
+    # Merender template dengan data cuaca
+    return render_template("index.html", data=weather_data)
